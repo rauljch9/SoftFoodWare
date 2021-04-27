d13fd1f6e2529e50469f3af45c19d95b732889fa
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-04-27 16:46:37 -0400

    Adding Getters and Setters to Ingredient Class

--- src/model/inventory/Ingredient.java
+++ src/model/inventory/Ingredient.java
@@ -8,4 +8,27 @@ public class Ingredient{
     String name;
     Double stock;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getStock() {
+        return stock;
+    }
+
+    public void setStock(Double stock) {
+        this.stock = stock;
+    }
 }
