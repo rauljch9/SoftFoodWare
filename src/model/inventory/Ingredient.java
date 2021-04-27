04cebd1694352e30a8cb1c6d0159627cca9f2247
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-04-27 16:41:06 -0400

    Fixing javadoc

--- src/model/inventory/Ingredient.java
+++ src/model/inventory/Ingredient.java
@@ -1,11 +1,11 @@
 package model.inventory;
 
 /**
- * This class will be used for the inventory and final products preparation
+ * This class will be used for the inventory and final products for the preparation
  */
 public class Ingredient{
     int id;
     String name;
     Double stock;
- s
+
 }
