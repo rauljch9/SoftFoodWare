4f11923555ea253033a6ec139d215d1c27ca594f
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-04-29 15:53:55 -0400

    Update Person.java
    Adding Getters and Setters

--- src/model/persons/Person.java
+++ src/model/persons/Person.java
@@ -10,4 +10,43 @@ public class Person {
     String phoneNumber;
     String address;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
