a3991291266da38b2577bc3ed362c689a75be49d
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-05-01 13:03:05 -0400

    Creating Client Gettes and Setters
    Clinet class extends from Person Class

---
+++ src/model/persons/Client.java
@@ -0,0 +1,10 @@
+package model.persons;
+
+import model.persons.Person;
+
+/**
+ * This represents the Clients that wil order
+ */
+public class Client extends Person {
+    String defaultDeliveryAddress;
+}
