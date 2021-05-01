c0ed076fc1d513e904a4e0d9c0c879c1f731ddfc
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-05-01 12:47:30 -0400

    Adding ProductDetail Getters and Setters

--- src/model/products/ProductDetail.java
+++ src/model/products/ProductDetail.java
@@ -9,4 +9,28 @@ public class ProductDetail {
     Ingredient ingredient;
     Double quantity;
     String note;
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
