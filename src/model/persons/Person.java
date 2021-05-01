b66873f212852e4cf139e382e3e46b18a2deaf63
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-05-01 13:02:03 -0400

    Creating Person Class

---
+++ src/model/persons/Person.java
@@ -0,0 +1,12 @@
+package model.persons;
+
+/**
+ * Class that will represent a general person
+ */
+public class Person {
+    int id;
+    String name;
+    String lastName;
+    String phoneNumber;
+    String address;
+}
