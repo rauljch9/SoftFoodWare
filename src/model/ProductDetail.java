1059d232e20689c3ba473d121cac0e9baa041cbe
rauljch9 <38894191+rauljch9@users.noreply.github.com>
2021-06-02 22:17:44 -0400

    Adding ProductDetail Class

---
+++ src/model/ProductDetail.java
@@ -0,0 +1,36 @@
+package model;
+
+import model.inventory.Ingredient;
+
+public class ProductDetail {
+    Ingredient ingredient;
+    Double quantity;
+    String note;
+
+    public ProductDetail() {
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
